77dbfed72560be1ce9cbcad7164f9b3713506441
Richard Knop <risoknop@gmail.com>
2015-11-09 22:33:54 +0800

    Improved unit tests.

--- oauth/utils_test.go
+++ oauth/utils_test.go
@@ -4,26 +4,43 @@ import (
 	"testing"
 
 	"database/sql"
+	"database/sql/driver"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestClientIDOrNull(t *testing.T) {
 	var nullInt64 sql.NullInt64
+	var value driver.Value
+	var err error
 
 	nullInt64 = clientIDOrNull(nil)
 	assert.False(t, nullInt64.Valid)
+	value, err = nullInt64.Value()
+	assert.Nil(t, err)
+	assert.Equal(t, nil, value)
 
 	nullInt64 = clientIDOrNull(&Client{ID: 1})
 	assert.True(t, nullInt64.Valid)
+	value, err = nullInt64.Value()
+	assert.Nil(t, err)
+	assert.Equal(t, int64(1), value)
 }
 
 func TestUserIDOrNull(t *testing.T) {
 	var nullInt64 sql.NullInt64
+	var value driver.Value
+	var err error
 
 	nullInt64 = userIDOrNull(nil)
 	assert.False(t, nullInt64.Valid)
+	value, err = nullInt64.Value()
+	assert.Nil(t, err)
+	assert.Equal(t, nil, value)
 
 	nullInt64 = userIDOrNull(&User{ID: 1})
 	assert.True(t, nullInt64.Valid)
+	value, err = nullInt64.Value()
+	assert.Nil(t, err)
+	assert.Equal(t, int64(1), value)
 }
