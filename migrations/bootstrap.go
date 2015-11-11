d5964b945f751040515327d8f2cba9e96742d0d6
Richard Knop <risoknop@gmail.com>
2015-11-12 02:20:42 +0800

    A little migrations refactoring.

---
+++ migrations/bootstrap.go
@@ -0,0 +1,43 @@
+package migrations
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/jinzhu/gorm"
+)
+
+// Bootstrap creates "migrations" table
+// to keep track of already run database migrations
+func Bootstrap(db *gorm.DB) error {
+	migrationName := "0000_bootstrap"
+
+	migration := new(Migration)
+	// Using Error instead of RecordNotFound because we want to check
+	// if the migrations table exists. This is different from later migrations
+	// where we query the already create migrations table.
+	exists := nil == db.LogMode(false).Where(Migration{
+		Name: migrationName,
+	}).First(migration).Error
+
+	if exists {
+		log.Printf("Skipping %s migration", migrationName)
+		return nil
+	}
+
+	log.Printf("Running %s migration", migrationName)
+
+	// Create migrations table
+	if err := db.CreateTable(new(Migration)).Error; err != nil {
+		return fmt.Errorf("Error creating migrations table: %s", db.Error)
+	}
+
+	// Save a record to migrations table,
+	// so we don't rerun this migration again
+	migration.Name = migrationName
+	if err := db.Create(migration).Error; err != nil {
+		return fmt.Errorf("Error saving record to migrations table: %s", err)
+	}
+
+	return nil
+}
--- migrations/migrations.go
+++ migrations/migrations.go
@@ -1,11 +1,7 @@
 package migrations
 
 import (
-	"fmt"
-	"log"
 	"time"
-
-	"github.com/jinzhu/gorm"
 )
 
 // Migration represents a single database migration
@@ -13,39 +9,4 @@ type Migration struct {
 	ID        int
 	Name      string `sql:"size:255"`
 	CreatedAt time.Time
-}
-
-// Bootstrap creates "migrations" table
-// to keep track of already run database migrations
-func Bootstrap(db *gorm.DB) error {
-	migrationName := "0000_bootstrap"
-
-	migration := new(Migration)
-	// Using Error instead of RecordNotFound because we want to check
-	// if the migrations table exists. This is different from later migrations
-	// where we query the already create migrations table.
-	exists := nil == db.LogMode(false).Where(Migration{
-		Name: migrationName,
-	}).First(migration).Error
-
-	if exists {
-		log.Printf("Skipping %s migration", migrationName)
-		return nil
-	}
-
-	log.Printf("Running %s migration", migrationName)
-
-	// Create migrations table
-	if err := db.CreateTable(new(Migration)).Error; err != nil {
-		return fmt.Errorf("Error creating migrations table: %s", db.Error)
-	}
-
-	// Save a record to migrations table,
-	// so we don't rerun this migration again
-	migration.Name = migrationName
-	if err := db.Create(migration).Error; err != nil {
-		return fmt.Errorf("Error saving record to migrations table: %s", err)
-	}
-
-	return nil
 }
