73720993186c4c20e5e7ee800c0bf8baf86c9e58
Richard Knop <risoknop@gmail.com>
2015-11-14 06:11:44 +0800

    Added few comments

--- oauth/scope_test.go
+++ oauth/scope_test.go
@@ -8,14 +8,18 @@ func (suite *OauthTestSuite) TestGetScope() {
 	var scope string
 	var err error
 
+	// When the requested scope is an empty string,
+	// the default scope should be returned
 	scope, err = suite.service.getScope("")
 	assert.Nil(suite.T(), err)
 	assert.Equal(suite.T(), "foo bar", scope)
 
+	// When the requested scope is valid, it should be returned
 	scope, err = suite.service.getScope("foo bar qux")
 	assert.Nil(suite.T(), err)
 	assert.Equal(suite.T(), "foo bar qux", scope)
 
+	// When the requested scope is invalid, an error should be returned
 	scope, err = suite.service.getScope("foo bar bogus")
 	if assert.NotNil(suite.T(), err) {
 		assert.Equal(suite.T(), "Invalid scope", err.Error())
