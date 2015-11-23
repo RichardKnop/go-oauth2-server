12a4495ddde3544dc51b09280035a03a9b931657
Richard Knop <risoknop@gmail.com>
2015-11-24 01:07:31 +0800

    Session test refactored a bit.

--- session/service_test.go
+++ session/service_test.go
@@ -15,18 +15,29 @@ import (
 // SessionTestSuite needs to be exported so the tests run
 type SessionTestSuite struct {
 	suite.Suite
+	service *Service
 }
 
 // The SetupSuite method will be run by testify once, at the very
 // start of the testing suite, before any tests are run.
 func (suite *SessionTestSuite) SetupSuite() {
-	//
+	// Overwrite internal vars so we don't affect existing session
+	storageSessionName = "test_session"
+	userSessionKey = "test_user"
+
+	// Initialise the service
+	r, err := http.NewRequest("GET", "http://1.2.3.4/foo/bar", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w := httptest.NewRecorder()
+	suite.service = NewService(config.NewConfig(), r, w)
 }
 
 // The TearDownSuite method will be run by testify once, at the very
 // end of the testing suite, after all tests have been run.
 func (suite *SessionTestSuite) TearDownSuite() {
-	//
+	suite.service.ClearUserSession()
 }
 
 // The SetupTest method will be run before every test in the suite.
@@ -40,31 +51,19 @@ func (suite *SessionTestSuite) TearDownTest() {
 }
 
 func (suite *SessionTestSuite) TestService() {
-	// Overwrite internal vars so we don't affect existing session
-	storageSessionName = "test_session"
-	userSessionKey = "test_user"
-
-	// Initialise the service
-	r, err := http.NewRequest("GET", "http://1.2.3.4/foo/bar", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	w := httptest.NewRecorder()
-	service := NewService(config.NewConfig(), r, w)
-
 	// No public methods should work before StartSession has been called
-	userSession, err := service.GetUserSession()
+	userSession, err := suite.service.GetUserSession()
 	if assert.NotNil(suite.T(), err) {
 		assert.Equal(suite.T(), errSessonNotStarted.Error(), err.Error())
 	}
 
 	// Call the StartSession method so internal session object gets set
-	if err := service.StartSession(); err != nil {
+	if err := suite.service.StartSession(); err != nil {
 		log.Fatal(err)
 	}
 
-	// Since SetUserSession has not been called yet, this should return an error
-	userSession, err = service.GetUserSession()
+	// Since StartSession has not been called yet, this should return an error
+	userSession, err = suite.service.GetUserSession()
 	if assert.NotNil(suite.T(), err) {
 		assert.Equal(suite.T(), "User session type assertion error", err.Error())
 	}
