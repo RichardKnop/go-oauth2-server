89c4c2c823a3b824d93027043b6564332f89cd5c
Richard Knop <risoknop@gmail.com>
2015-11-24 18:48:00 +0800

    More thorough session test.

--- session/service_test.go
+++ session/service_test.go
@@ -14,12 +14,15 @@ import (
 // SessionTestSuite needs to be exported so the tests run
 type SessionTestSuite struct {
 	suite.Suite
+	cnf     *config.Config
 	service *Service
 }
 
 // The SetupSuite method will be run by testify once, at the very
 // start of the testing suite, before any tests are run.
 func (suite *SessionTestSuite) SetupSuite() {
+	suite.cnf = config.NewConfig()
+
 	// Overwrite internal vars so we don't affect existing session
 	storageSessionName = "test_session"
 	userSessionKey = "test_user"
@@ -30,13 +33,13 @@ func (suite *SessionTestSuite) SetupSuite() {
 		log.Fatal(err)
 	}
 	w := httptest.NewRecorder()
-	suite.service = NewService(config.NewConfig(), r, w)
+	suite.service = NewService(suite.cnf, r, w)
 }
 
 // The TearDownSuite method will be run by testify once, at the very
 // end of the testing suite, after all tests have been run.
 func (suite *SessionTestSuite) TearDownSuite() {
-	suite.service.ClearUserSession()
+	//
 }
 
 // The SetupTest method will be run before every test in the suite.
@@ -52,6 +55,7 @@ func (suite *SessionTestSuite) TearDownTest() {
 func (suite *SessionTestSuite) TestService() {
 	// No public methods should work before StartSession has been called
 	userSession, err := suite.service.GetUserSession()
+	assert.Nil(suite.T(), userSession)
 	if assert.NotNil(suite.T(), err) {
 		assert.Equal(suite.T(), errSessonNotStarted.Error(), err.Error())
 	}
@@ -61,13 +65,45 @@ func (suite *SessionTestSuite) TestService() {
 		log.Fatal(err)
 	}
 
-	// Since StartSession has not been called yet, this should return an error
+	// Let's clear the user session first
+	err = suite.service.ClearUserSession()
+	assert.Nil(suite.T(), err)
+
+	// Since the user session has not been set yet, this should return error
 	userSession, err = suite.service.GetUserSession()
+	assert.Nil(suite.T(), userSession)
 	if assert.NotNil(suite.T(), err) {
 		assert.Equal(suite.T(), "User session type assertion error", err.Error())
 	}
 
-	log.Print(userSession)
+	// Let's set the user session now
+	suite.service.SetUserSession(&UserSession{
+		ClientID:     "test_client",
+		Username:     "test@username",
+		AccessToken:  "test_access_token",
+		RefreshToken: "test_refresh_token",
+	})
+
+	// User session is set, this should return it
+	userSession, err = suite.service.GetUserSession()
+	assert.Nil(suite.T(), err)
+	if assert.NotNil(suite.T(), userSession) {
+		assert.Equal(suite.T(), "test_client", userSession.ClientID)
+		assert.Equal(suite.T(), "test@username", userSession.Username)
+		assert.Equal(suite.T(), "test_access_token", userSession.AccessToken)
+		assert.Equal(suite.T(), "test_refresh_token", userSession.RefreshToken)
+	}
+
+	// Let's clear the user session now
+	err = suite.service.ClearUserSession()
+	assert.Nil(suite.T(), err)
+
+	// Since the user session has been cleared, this should return error
+	userSession, err = suite.service.GetUserSession()
+	assert.Nil(suite.T(), userSession)
+	if assert.NotNil(suite.T(), err) {
+		assert.Equal(suite.T(), "User session type assertion error", err.Error())
+	}
 }
 
 // TesSessionTestSuite ...
