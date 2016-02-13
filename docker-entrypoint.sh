692c3aeb93fafd13d8560b05f740fa6d0998f8a2
Richard Knop <risoknop@gmail.com>
2016-02-13 23:37:53 +0800

    Added loading of scopes fixture to docker entrypoint.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,5 +3,9 @@
 # 1. Run database migrations
 /go/bin/go-oauth2-server migrate
 
+# 2. Load fixtures
+/go/bin/go-oauth2-server loaddata \
+  oauth/fixtures/scopes.yml
+
 # Finally, run the server
 /go/bin/go-oauth2-server runserver
