1507411fd4adddc090b3e3a3f30a1063293536d3
Richard Knop <RichardKnop@users.noreply.github.com>
2016-02-28 22:43:47 +0800

    Update response.go

--- response/response.go
+++ response/response.go
@@ -6,13 +6,18 @@ import (
 	"net/http"
 )
 
-var realm = "go_oauth2_server"
+const realm = "go_oauth2_server"
 
 // WriteJSON writes JSON response
 func WriteJSON(w http.ResponseWriter, v interface{}, code int) {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(code)
 	json.NewEncoder(w).Encode(v)
+}
+
+// NoContent writes a 204 no content response
+func NoContent(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusNoContent)
 }
 
 // Error produces a JSON error response with the following structure:
