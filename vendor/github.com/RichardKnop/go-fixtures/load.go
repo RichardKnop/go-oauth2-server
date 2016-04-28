5ed61e6edba2ee2da170d8ece4023a3e43a11d12
Richard Knop <risoknop@gmail.com>
2016-04-28 18:10:22 +0800

    Updated dependencies.

---
+++ vendor/github.com/RichardKnop/go-fixtures/load.go
@@ -0,0 +1,163 @@
+package fixtures
+
+import (
+	"database/sql"
+	"fmt"
+	"io/ioutil"
+	"strings"
+
+	"gopkg.in/yaml.v2"
+)
+
+func NewProcessingError(row int, cause error) error {
+	return fmt.Errorf("Error loading row %d: %s", row, cause.Error())
+}
+
+func NewFileError(filename string, cause error) error {
+	return fmt.Errorf("Error loading file %s: %s", filename, cause.Error())
+}
+
+// Load processes a YAML fixture and inserts/updates the database accordingly
+func Load(data []byte, db *sql.DB, driver string) error {
+	// Unmarshal the YAML data into a []Row slice
+	var rows []Row
+	if err := yaml.Unmarshal(data, &rows); err != nil {
+		return err
+	}
+
+	// Begin a transaction
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+
+	// Iterate over rows define in the fixture
+	for i, row := range rows {
+		// Load internat struct variables
+		row.Init()
+
+		// Run a SELECT query to find out if we need to insert or UPDATE
+		selectQuery := fmt.Sprintf(
+			"SELECT COUNT(*) FROM %s WHERE %s",
+			row.Table,
+			row.GetWhere(driver, 0),
+		)
+		var count int
+		err = tx.QueryRow(selectQuery, row.GetPKValues()...).Scan(&count)
+		if err != nil {
+			tx.Rollback() // rollback the transaction
+			return NewProcessingError(i+1, err)
+		}
+
+		if count == 0 {
+			// Primary key not found, let's run an INSERT query
+			insertQuery := fmt.Sprintf(
+				"INSERT INTO %s(%s) VALUES(%s)",
+				row.Table,
+				strings.Join(row.GetInsertColumns(), ", "),
+				strings.Join(row.GetInsertPlaceholders(driver), ", "),
+			)
+			_, err := tx.Exec(insertQuery, row.GetInsertValues()...)
+			if err != nil {
+				tx.Rollback() // rollback the transaction
+				return NewProcessingError(i+1, err)
+			}
+			if driver == postgresDriver && row.GetInsertColumns()[0] == "id" {
+
+				var dtype string
+				err = tx.QueryRow(checkPostgresPKDataType(row.Table)).Scan(&dtype)
+				if err != nil {
+					tx.Rollback() // rollback the transaction
+					return NewProcessingError(i+1, err)
+				}
+
+				if dtype == "integer" {
+					// Fixed the primary ID sequence for Postgres
+					_, err := tx.Exec(fixPostgresPKSequence(row.Table))
+					if err != nil {
+						tx.Rollback() // rollback the transaction
+						return NewProcessingError(i+1, err)
+					}
+				}
+			}
+		} else {
+			// Primary key found, let's run UPDATE query
+			updateQuery := fmt.Sprintf(
+				"UPDATE %s SET %s WHERE %s",
+				row.Table,
+				strings.Join(row.GetUpdatePlaceholders(driver), ", "),
+				row.GetWhere(driver, row.GetUpdateColumnsLength()),
+			)
+			values := append(row.GetUpdateValues(), row.GetPKValues()...)
+			_, err := tx.Exec(updateQuery, values...)
+			if err != nil {
+				tx.Rollback() // rollback the transaction
+				return NewProcessingError(i+1, err)
+			}
+			if driver == postgresDriver && row.GetUpdateColumns()[0] == "id" {
+				var dtype string
+				err = tx.QueryRow(checkPostgresPKDataType(row.Table)).Scan(&dtype)
+				if err != nil {
+					tx.Rollback() // rollback the transaction
+					return NewProcessingError(i+1, err)
+				}
+
+				if dtype == "integer" {
+					// Fixed the primary ID sequence for Postgres
+					_, err := tx.Exec(fixPostgresPKSequence(row.Table))
+					if err != nil {
+						tx.Rollback() // rollback the transaction
+						return NewProcessingError(i+1, err)
+					}
+				}
+			}
+		}
+	}
+
+	// Commit the transaction
+	if err := tx.Commit(); err != nil {
+		tx.Rollback() // rollback the transaction
+		return err
+	}
+
+	return nil
+}
+
+func checkPostgresPKDataType(table string) string {
+	return fmt.Sprintf(
+		"SELECT data_type "+
+			"FROM information_schema.columns WHERE table_name='%s' "+
+			"AND column_name='id';",
+		table,
+	)
+}
+
+// fixPostgresPKSequence resets primary key sequence after manual insertion
+func fixPostgresPKSequence(table string) string {
+	return fmt.Sprintf(
+		"SELECT pg_catalog.setval(pg_get_serial_sequence('%s', 'id'), "+
+			"(SELECT MAX(id) FROM %s));",
+		table,
+		table,
+	)
+}
+
+func LoadFile(filename string, db *sql.DB, driver string) error {
+	// Read fixture data from the file
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return NewFileError(filename, err)
+	}
+
+	// Insert the fixture data
+	return Load(data, db, driver)
+}
+
+func LoadFiles(filenames []string, db *sql.DB, driver string) error {
+	for _, filename := range filenames {
+		if err := LoadFile(filename, db, driver); err != nil {
+			return err
+		}
+	}
+	return nil
+}
---
+++ vendor/github.com/RichardKnop/go-fixtures/row.go
@@ -0,0 +1,166 @@
+package fixtures
+
+import (
+	"fmt"
+	"sort"
+	"strings"
+	"time"
+)
+
+const (
+	onInsertNow    = "ON_INSERT_NOW()"
+	onUpdateNow    = "ON_UPDATE_NOW()"
+	postgresDriver = "postgres"
+)
+
+// Row represents a single database row
+type Row struct {
+	Table              string
+	PK                 map[string]interface{}
+	Fields             map[string]interface{}
+	insertColumnLength int
+	updateColumnLength int
+	pkColumns          []string
+	pkValues           []interface{}
+	insertColumns      []string
+	updateColumns      []string
+	insertValues       []interface{}
+	updateValues       []interface{}
+}
+
+// Init loads internal struct variables
+func (row *Row) Init() {
+	// Initial values
+	row.insertColumnLength = len(row.PK) + len(row.Fields)
+	row.updateColumnLength = len(row.PK) + len(row.Fields)
+	row.pkColumns = make([]string, 0)
+	row.pkValues = make([]interface{}, 0)
+	row.insertColumns = make([]string, 0)
+	row.updateColumns = make([]string, 0)
+	row.insertValues = make([]interface{}, 0)
+	row.updateValues = make([]interface{}, 0)
+
+	// Get and sort map keys
+	var i int
+	pkKeys := make([]string, len(row.PK))
+	i = 0
+	for pkKey := range row.PK {
+		pkKeys[i] = pkKey
+		i++
+	}
+	sort.Strings(pkKeys)
+	fieldKeys := make([]string, len(row.Fields))
+	i = 0
+	for fieldKey := range row.Fields {
+		fieldKeys[i] = fieldKey
+		i++
+	}
+	sort.Strings(fieldKeys)
+
+	// Primary keys
+	for _, pkKey := range pkKeys {
+		row.pkColumns = append(row.pkColumns, pkKey)
+		row.pkValues = append(row.pkValues, row.PK[pkKey])
+		row.insertColumns = append(row.insertColumns, pkKey)
+		row.updateColumns = append(row.updateColumns, pkKey)
+		row.insertValues = append(row.insertValues, row.PK[pkKey])
+		row.updateValues = append(row.updateValues, row.PK[pkKey])
+	}
+
+	// Rest of the fields
+	for _, fieldKey := range fieldKeys {
+		sv, ok := row.Fields[fieldKey].(string)
+		if ok && sv == onInsertNow {
+			row.insertColumns = append(row.insertColumns, fieldKey)
+			row.insertValues = append(row.insertValues, time.Now())
+			row.updateColumnLength--
+			continue
+		}
+		if ok && sv == onUpdateNow {
+			row.updateColumns = append(row.updateColumns, fieldKey)
+			row.updateValues = append(row.updateValues, time.Now())
+			row.insertColumnLength--
+			continue
+		}
+		row.insertColumns = append(row.insertColumns, fieldKey)
+		row.updateColumns = append(row.updateColumns, fieldKey)
+		row.insertValues = append(row.insertValues, row.Fields[fieldKey])
+		row.updateValues = append(row.updateValues, row.Fields[fieldKey])
+	}
+}
+
+// GetInsertColumnsLength returns number of columns for INSERT query
+func (row *Row) GetInsertColumnsLength() int {
+	return row.insertColumnLength
+}
+
+// GetUpdateColumnsLength returns number of columns for UDPATE query
+func (row *Row) GetUpdateColumnsLength() int {
+	return row.updateColumnLength
+}
+
+// GetInsertColumns returns a slice of column names for INSERT query
+func (row *Row) GetInsertColumns() []string {
+	return row.insertColumns
+}
+
+// GetUpdateColumns returns a slice of column names for UPDATE query
+func (row *Row) GetUpdateColumns() []string {
+	return row.updateColumns
+}
+
+// GetInsertValues returns a slice of values for INSERT query
+func (row *Row) GetInsertValues() []interface{} {
+	return row.insertValues
+}
+
+// GetUpdateValues returns a slice of values for UPDATE query
+func (row *Row) GetUpdateValues() []interface{} {
+	return row.updateValues
+}
+
+// GetInsertPlaceholders returns a slice of placeholders for INSERT query
+func (row *Row) GetInsertPlaceholders(driver string) []string {
+	placeholders := make([]string, row.GetInsertColumnsLength())
+	for i := range row.insertColumns {
+		if driver == postgresDriver {
+			placeholders[i] = fmt.Sprintf("$%d", i+1)
+		} else {
+			placeholders[i] = "?"
+		}
+	}
+	return placeholders
+}
+
+// GetUpdatePlaceholders returns a slice of placeholders for UPDATE query
+func (row *Row) GetUpdatePlaceholders(driver string) []string {
+	placeholders := make([]string, row.GetUpdateColumnsLength())
+	for i, c := range row.updateColumns {
+		if driver == postgresDriver {
+			placeholders[i] = fmt.Sprintf("%s = $%d", c, i+1)
+		} else {
+			placeholders[i] = fmt.Sprintf("%s = ?", c)
+		}
+	}
+	return placeholders
+}
+
+// GetWhere returns a where condition based on primary key with placeholders
+func (row *Row) GetWhere(driver string, i int) string {
+	wheres := make([]string, len(row.PK))
+	j := i
+	for _, c := range row.pkColumns {
+		if driver == postgresDriver {
+			wheres[i-j] = fmt.Sprintf("%s = $%d", c, i+1)
+		} else {
+			wheres[i-j] = fmt.Sprintf("%s = ?", c)
+		}
+		i++
+	}
+	return strings.Join(wheres, " AND ")
+}
+
+// GetPKValues returns a slice of primary key values
+func (row *Row) GetPKValues() []interface{} {
+	return row.pkValues
+}
