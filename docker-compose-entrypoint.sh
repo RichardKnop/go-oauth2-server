35ba85777d2e690d619cbfbdaffaa8631e5fcce2
Richard Knop <RichardKnop@users.noreply.github.com>
2016-09-20 15:54:56 +0800

    Update docker-compose-entrypoint.sh

--- docker-compose-entrypoint.sh
+++ docker-compose-entrypoint.sh
@@ -30,7 +30,6 @@ if [ "$1" = 'go-oauth2-server' ] && [ "$2" = 'runserver' ]; then
   $1 migrate
   $1 loaddata oauth/fixtures/scopes.yml
   $1 loaddata oauth/fixtures/test_clients.yml
-  $1 loaddata accounts/fixtures/roles.yml
 fi
 
 exec "$@"
