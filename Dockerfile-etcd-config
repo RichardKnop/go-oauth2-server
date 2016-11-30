a95971de41141fb75065457ba5d85d12d154eb59
Richard Knop <risoknop@gmail.com>
2016-11-30 17:38:11 +0800

    docker compose refactor

---
+++ Dockerfile-etcd-config
@@ -0,0 +1,3 @@
+FROM quay.io/coreos/etcd
+COPY ./docker-etcd-config-entrypoint.sh /
+ENTRYPOINT ["/docker-etcd-config-entrypoint.sh"]
---
+++ docker-etcd-config-entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# to make sure etcd is ready (election ended and leader elected)
+while ! etcdctl endpoint health &>/dev/null; do :; done
+
+exec etcdctl put /config/go_oauth2_server.json '{
+  "Database": {
+    "Type": "postgres",
+    "Host": "postgres",
+    "Port": 5432,
+    "User": "go_oauth2_server",
+    "Password": "",
+    "DatabaseName": "go_oauth2_server",
+    "MaxIdleConns": 5,
+    "MaxOpenConns": 5
+  },
+  "Oauth": {
+    "AccessTokenLifetime": 3600,
+    "RefreshTokenLifetime": 1209600,
+    "AuthCodeLifetime": 3600
+  },
+  "Session": {
+      "Secret": "test_secret",
+      "Path": "/",
+      "MaxAge": 604800,
+      "HTTPOnly": true
+  },
+  "IsDevelopment": true
+}'
