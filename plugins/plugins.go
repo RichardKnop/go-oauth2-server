4ed5c4d60fb266e2f6348fe287925ca241c2e61a
Adam Hanna <ahanna@alumni.mines.edu>
2017-04-19 18:20:31 -0700

    moved bulk of redis integration to new file in plugins dir

--- plugins/plugins.go
+++ plugins/plugins.go
@@ -1,8 +1,6 @@
 package plugins
 
 import (
-	"errors"
-	"fmt"
 	"log"
 	"net/http"
 
@@ -19,6 +17,7 @@ import (
 // CustomHealthService extends health.ServiceInterface
 type CustomHealthService struct {
 	health.ServiceInterface
+	db *gorm.DB
 }
 
 // NewHealthService defines a custom health service if the developer so chooses to implement one
@@ -27,9 +26,12 @@ func NewHealthService(db *gorm.DB) *CustomHealthService {
 	return nil
 }
 
-// CustomAuthService extends health.ServiceInterface
+// CustomAuthService extends oauth.ServiceInterface
 type CustomAuthService struct {
 	oauth.ServiceInterface
+	cnf          *config.Config
+	db           *gorm.DB
+	allowedRoles []string
 }
 
 // NewOauthService defines a custom auth service if the developer so chooses to implement one
@@ -38,7 +40,7 @@ func NewOauthService(cnf *config.Config, db *gorm.DB) *CustomAuthService {
 	return nil
 }
 
-// CustomSessionService extends health.ServiceInterface
+// CustomSessionService extends session.ServiceInterface
 type CustomSessionService struct {
 	session.ServiceInterface
 	sessionStore   sessions.Store
@@ -46,90 +48,6 @@ type CustomSessionService struct {
 	session        *sessions.Session
 	r              *http.Request
 	w              http.ResponseWriter
-}
-
-// SetSessionService custom SetSessionStore
-func (c *CustomSessionService) SetSessionService(r *http.Request, w http.ResponseWriter) {
-	c.r = r
-	c.w = w
-}
-
-// StartSession custom StartSession
-func (c *CustomSessionService) StartSession() error {
-	// Get a session.
-	session, err := c.sessionStore.Get(c.r, session.UserSessionKey)
-	if err != nil {
-		return err
-	}
-
-	c.session = session
-	return nil
-}
-
-// GetUserSession custom GetUserSession
-func (c *CustomSessionService) GetUserSession() (*session.UserSession, error) {
-	// Make sure StartSession has been called
-	if c.session == nil {
-		return nil, session.ErrSessonNotStarted
-	}
-
-	// Retrieve our user session struct and type-assert it
-	userSession, ok := c.session.Values[session.UserSessionKey].(*session.UserSession)
-	if !ok {
-		return nil, errors.New("User session type assertion error")
-	}
-
-	return userSession, nil
-}
-
-// SetUserSession custom SetUserSession
-func (c *CustomSessionService) SetUserSession(userSession *session.UserSession) error {
-	// Make sure StartSession has been called
-	if c.session == nil {
-		return session.ErrSessonNotStarted
-	}
-
-	// Set a new user session
-	c.session.Values[session.UserSessionKey] = userSession
-	return c.session.Save(c.r, c.w)
-}
-
-// ClearUserSession custom ClearUserSession
-func (c *CustomSessionService) ClearUserSession() error {
-	c.session.Options.MaxAge = -1
-	return c.session.Save(c.r, c.w)
-}
-
-// SetFlashMessage custom SetFlashMessage
-func (c *CustomSessionService) SetFlashMessage(msg string) error {
-	// Make sure StartSession has been called
-	if c.session == nil {
-		return session.ErrSessonNotStarted
-	}
-
-	// Add the flash message
-	c.session.AddFlash(msg)
-	return c.session.Save(c.r, c.w)
-}
-
-// GetFlashMessage custom GetFlashMessage
-func (c *CustomSessionService) GetFlashMessage() (interface{}, error) {
-	// Make sure StartSession has been called
-	if c.session == nil {
-		return nil, session.ErrSessonNotStarted
-	}
-
-	// Get the last flash message from the stack
-	fmt.Println("b4 flashes in plugins")
-	if flashes := c.session.Flashes(); len(flashes) > 0 {
-		fmt.Println("Plugins flashes", flashes)
-		// We need to save the session, otherwise the flash message won't be removed
-		c.session.Save(c.r, c.w)
-		return flashes[0], nil
-	}
-
-	// No flash messages in the stack
-	return nil, nil
 }
 
 // NewSessionService defines a custom session service if the developer so chooses to implement one
@@ -153,9 +71,12 @@ func NewSessionService(cnf *config.Config) *CustomSessionService {
 	}
 }
 
-// CustomWebService extends health.ServiceInterface
+// CustomWebService extends web.ServiceInterface
 type CustomWebService struct {
 	web.ServiceInterface
+	cnf            *config.Config
+	oauthService   oauth.ServiceInterface
+	sessionService session.ServiceInterface
 }
 
 // NewWebService defines a custom web service if the developer so chooses to implement one
