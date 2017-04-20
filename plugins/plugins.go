3ed1ca57eb96713180171462c2a462512433bc52
Adam Hanna <ahanna@alumni.mines.edu>
2017-04-19 17:30:37 -0700

    no build errs

--- plugins/plugins.go
+++ plugins/plugins.go
@@ -47,14 +47,16 @@ type CustomSessionService struct {
 	w              http.ResponseWriter
 }
 
+// SetSessionService custom SetSessionStore
 func (c *CustomSessionService) SetSessionService(r *http.Request, w http.ResponseWriter) {
 	c.r = r
 	c.w = w
 }
 
+// StartSession custom StartSession
 func (c *CustomSessionService) StartSession() error {
 	// Get a session.
-	session, err := store.Get(c.r, session.UserSessionKey)
+	session, err := c.sessionStore.Get(c.r, session.UserSessionKey)
 	if err != nil {
 		return err
 	}
@@ -63,6 +65,7 @@ func (c *CustomSessionService) StartSession() error {
 	return nil
 }
 
+// GetUserSession custom GetUserSession
 func (c *CustomSessionService) GetUserSession() (*session.UserSession, error) {
 	// Make sure StartSession has been called
 	if c.session == nil {
@@ -78,28 +81,52 @@ func (c *CustomSessionService) GetUserSession() (*session.UserSession, error) {
 	return userSession, nil
 }
 
+// SetUserSession custom SetUserSession
 func (c *CustomSessionService) SetUserSession(userSession *session.UserSession) error {
 	// Make sure StartSession has been called
 	if c.session == nil {
-		return ErrSessonNotStarted
+		return session.ErrSessonNotStarted
 	}
 
 	// Set a new user session
 	c.session.Values[session.UserSessionKey] = userSession
-	return c.session.Save(s.r, s.w)
+	return c.session.Save(c.r, c.w)
 }
 
+// ClearUserSession custom ClearUserSession
 func (c *CustomSessionService) ClearUserSession() error {
 	c.session.Options.MaxAge = -1
-	return c.sessions.Save(s.r, s.w)
+	return c.session.Save(c.r, c.w)
 }
 
+// SetFlashMessage custom SetFlashMessage
 func (c *CustomSessionService) SetFlashMessage(msg string) error {
+	// Make sure StartSession has been called
+	if c.session == nil {
+		return session.ErrSessonNotStarted
+	}
 
+	// Add the flash message
+	c.session.AddFlash(msg)
+	return c.session.Save(c.r, c.w)
 }
 
+// GetFlashMessage custom GetFlashMessage
 func (c *CustomSessionService) GetFlashMessage() (interface{}, error) {
+	// Make sure StartSession has been called
+	if c.session == nil {
+		return nil, session.ErrSessonNotStarted
+	}
 
+	// Get the last flash message from the stack
+	if flashes := c.session.Flashes(); len(flashes) > 0 {
+		// We need to save the session, otherwise the flash message won't be removed
+		c.session.Save(c.r, c.w)
+		return flashes[0], nil
+	}
+
+	// No flash messages in the stack
+	return nil, nil
 }
 
 // NewSessionService defines a custom session service if the developer so chooses to implement one
