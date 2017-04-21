f94b88256ac4cfda5e0422778eea1df796c25799
Adam Hanna <ahanna@alumni.mines.edu>
2017-04-21 08:47:20 -0700

    removed unused vars

--- cmd/util.go
+++ cmd/util.go
@@ -3,18 +3,7 @@ package cmd
 import (
 	"github.com/adam-hanna/go-oauth2-server/config"
 	"github.com/adam-hanna/go-oauth2-server/database"
-	"github.com/adam-hanna/go-oauth2-server/health"
-	"github.com/adam-hanna/go-oauth2-server/oauth"
-	"github.com/adam-hanna/go-oauth2-server/session"
-	"github.com/adam-hanna/go-oauth2-server/web"
 	"github.com/jinzhu/gorm"
-)
-
-var (
-	healthService  health.ServiceInterface
-	oauthService   oauth.ServiceInterface
-	webService     web.ServiceInterface
-	sessionService session.ServiceInterface
 )
 
 // initConfigDB loads the configuration and connects to the database
