14de713fd42bc731c60c973530fa91d65ac21177
Adam Hanna <ahanna@alumni.mines.edu>
2017-04-21 08:37:31 -0700

    finished refactoring abstraction

--- cmd/run_server.go
+++ cmd/run_server.go
@@ -5,7 +5,6 @@ import (
 	"time"
 
 	"github.com/adam-hanna/go-oauth2-server/services"
-	"github.com/adam-hanna/redis-sessions/redis"
 	"github.com/gorilla/mux"
 	"github.com/phyber/negroni-gzip/gzip"
 	"github.com/urfave/negroni"
@@ -20,19 +19,7 @@ func RunServer(configBackend string) error {
 	}
 	defer db.Close()
 
-	// configure redis for session store
-	sessionSecrets := make([][]byte, 1)
-	sessionSecrets[0] = []byte(cnf.Session.Secret)
-	redisConfig := redis.ConfigType{
-		Size:           10,
-		Network:        "tcp",
-		Address:        ":6379",
-		Password:       "",
-		SessionSecrets: sessionSecrets,
-	}
-
 	// start the services
-	services.UseSessionService(redis.NewService(cnf, redisConfig))
 	if err := services.InitServices(cnf, db); err != nil {
 		return err
 	}
